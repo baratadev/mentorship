d1a8090f5fd43e02d5612b87676efd33a3e9de41
Barata <8771928+baratadev@users.noreply.github.com>
2022-02-22 17:12:46 -0300

    add APP route

--- APP/index.js
+++ APP/index.js
@@ -6,14 +6,52 @@ const fs = require('fs');
 // server
 const server = http.createServer((req, res) => {
   // request
-  console.log(req.url, req.method);
+  const url = req.url;
+  const method = req.method;
+  console.log(url, method);
 
   // response
   // set header content-type
   res.setHeader('Content-Type', 'text/html');
 
+  // set path for html pages
+  let urlPath = './pages/';
+
+  // load html pages
+  switch (url) {
+    // home page
+    case '/':
+      urlPath += 'index.html';
+      break;
+
+    // users endpoint
+    case '/users':
+      if (method === 'GET') {
+        urlPath += '/users/users.html';
+        res.statusCode = 200;
+      } else if (method === 'POST') {
+        urlPath += '/users/add-user.html';
+        res.statusCode = 200;
+      } else if (method === 'PUT') {
+        urlPath += '/users/edit-user.html';
+        res.statusCode = 200;
+      } else if (method === 'DELETE') {
+        urlPath += '/users/delete-user.html';
+        res.statusCode = 200;
+      } else {
+        console.log('405 - METHOD NOT ALLOWED');
+        res.statusCode = 405;
+      }
+      break;
+    // 404 page
+    default:
+      urlPath += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
   // read home page
-  fs.readFile('./index.html', (err, data) => {
+  fs.readFile(urlPath, (err, data) => {
     if (err) {
       console.log(`SOMETHING WENT WRONG: ${err}`);
       res.end();
